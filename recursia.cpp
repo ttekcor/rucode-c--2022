68234e9dd4f4b9bb340fde73b72c0b8cf460e59d
Kostya Paranko <85117608+ttekcor@users.noreply.github.com>
2022-11-26 03:55:20 +1000

    Рекурсия
    Очень важно!!!

---
+++ recursia.cpp
@@ -0,0 +1,24 @@
+﻿
+#include <iostream>
+
+    using namespace std;
+void func(string str, int len)
+    {
+        if (!len)
+        {
+            cout << str << endl;
+            return;
+        }
+        func(str + '0', len - 1);
+        func(str + '1', len - 1);
+        
+    }
+int main()
+    {
+        int n;
+
+        cin >> n;
+
+        func("", n);
+        return 0;
+    }
