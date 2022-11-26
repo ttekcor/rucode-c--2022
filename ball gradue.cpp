6398262ef9603069a17ebe32b20c5147ff7811aa
Kostya Paranko <85117608+ttekcor@users.noreply.github.com>
2022-11-27 03:04:07 +1000

    Мячик на ступеньках

---
+++ ball gradue.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[31] = { 0 };
+    a[n] = 1;
+    for (int i = n; i >= 0; i--) {
+        if (i - 1 >= 0) a[i - 1] += a[i];
+        if (i - 2 >= 0) a[i - 2] += a[i];
+        if (i - 3 >= 0) a[i - 3] += a[i];
+    }
+    cout << a[0];
+    return 0;
+}
